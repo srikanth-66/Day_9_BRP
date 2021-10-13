08fe765feb0d8cd473043f39f3ae2735d863391e
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-10-13 14:28:21 +0530

    Adding the program which check the given number is prime or not

---
+++ CheckingIsPrime.java
@@ -0,0 +1,35 @@
+package com.classroom.bootcamp;
+
+import java.util.Scanner;
+
+public class CheckingIsPrime 
+{
+    static boolean isPrime(int n)
+    {
+    	if (n <= 1)
+            return false;
+        else if (n == 2)
+            return true;
+        else if (n % 2 == 0)
+            return false;
+ 
+        for (int i = 3; i <= Math.sqrt(n); i += 2)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+     public static void main(String[] args)
+    {
+    	 Scanner sc=new Scanner(System.in);
+    	 System.out.println("enter the number to check");
+    	 int n=sc.nextInt()
+;        if (isPrime(n))
+            System.out.println("true");
+ 
+        else
+            System.out.println("false");
+    }
+
+}
